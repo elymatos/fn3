bee71b8f4aaf669a6e519b739e94d9f387501b84
Ely Edison Matos <ely.matos@gmail.com>
2025-06-24 22:36:44 -0300

    Quartz sync: Jun 24, 2025, 10:36 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,5 @@
 ---
-title: Welcome to Quartz
+title: FN3
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+OntoLexical FrameNet
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "FN5",
+    pageTitle: "FN3",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "elymatos.github.io/fn5/",
+    baseUrl: "elymatos.github.io/fn3/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
